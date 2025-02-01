08e7ad31540de83a9bfbeb261d1cb65cfb7c1953
roufai-ne <roufay_amadou@yahoo.fr>
2025-02-01 12:10:35 +0100

    V3

--- src/components/admin/CommunicationsManager.js
+++ src/components/admin/CommunicationsManager.js
@@ -1,5 +1,5 @@
 // src/pages/admin/communications.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback} from 'react';
 import { 
   Bell, 
   Calendar,
